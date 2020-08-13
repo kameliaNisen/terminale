3135356ac1160b735db2c687343640ba95a67ba6
seynave <r.seynave59@gmail.com>
2020-08-14 01:03:57 +0200

    creation fichier eleves

---
+++ bloc1/POO/mini_projet_jeu_de_carte/joueur.py
@@ -0,0 +1,16 @@
+from jeuDeCarte import *
+
+class Joueur:
+    def __init__(self, nom, main):
+        #  main : liste de cartes que le joueur possède
+        # A compléter
+
+    def jouerCarte(self,i):
+        # A compléter
+    
+    def __repr__(self):
+        # A compléter
+        
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(optionflags=doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS, verbose=False)        
